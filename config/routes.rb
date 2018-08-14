84ea5fb97416dd531de5503fe8122099fcc444aa
Julia Campana <juliarcampana@gmail.com>
2018-08-14 12:56:48 -0300

    Create scooter's controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'scooters/index'
+  get 'scooters/show'
+  get 'scooters/new'
+  get 'scooters/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
