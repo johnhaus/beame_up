0ad35bfe152f27550f7c5771efeab2e1bb838b5c
Julia Campana <juliarcampana@gmail.com>
2018-08-14 14:58:30 -0300

    Add new and create methods to scooter's controller

--- app/controllers/scooters_controller.rb
+++ app/controllers/scooters_controller.rb
@@ -1,4 +1,6 @@
 class ScootersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
   end
 
@@ -6,9 +8,18 @@ class ScootersController < ApplicationController
   end
 
   def new
+    @scooter = Scooter.new
   end
 
   def create
+    @scooter = Scooter.new(scooter_params)
+    @scooter.user = current_user
+
+    if @scooter.save
+      redirect_to @scooter
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +30,11 @@ class ScootersController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def scooter_params
+    params.require(:scooter).permit(:name, :description)
+  end
+
 end
